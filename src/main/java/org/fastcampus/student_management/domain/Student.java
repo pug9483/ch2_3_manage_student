c31a4e7a50ecc40df4d1e92c96ecf0b40cc880c5
pug9483 <2019136060@koreatech.ac.kr>
2025-04-21 23:13:32 +0900

    feat : 수강생들의 상태를 변경한다.
    - 학생이 활성 상태 일 때에는 일별 수업에서 포함이 되어야 함
    - 학생이 비활성 상태일 때에는, 일별 수업에 포함되서는 안 됨
    - 활동 상태에서 활동 상태로, 비활성 상태에서 비활성 상태로 변경이 되어서는 안 됨

--- src/main/java/org/fastcampus/student_management/domain/Student.java
+++ src/main/java/org/fastcampus/student_management/domain/Student.java
@@ -18,6 +18,40 @@ public class Student {
     this.activated = true;
   }
 
+  /**
+   * setActivated가 안티 패턴인 이유
+   * 1. setter는 캡슐화를 떨어뜨리기 때문에 권장되지 않는다. (dto 외에는 setter 사용을 지양해야 한다.)
+   * 2. 코드의 가독성이 떨어지기 때문에 유즈케이스가 코드 내에서도 불분명해지며 안정성이 떨어진다.
+   */
+  /*public void setActivated(boolean activated) {
+    if (activated && this.activated) {
+      throw new IllegalArgumentException();
+    }
+
+    if (!activated && !this.activated) {
+      throw new IllegalArgumentException();
+    }
+
+    this.activated = activated;
+  }*/
+
+  /**
+   * 정확하게 어떤 동작을 할지 메서드 명으로 알 수 있기 때문에 가독성이 좋아진다.
+   */
+  public void activate(){
+    if (this.activated) {
+      throw new IllegalArgumentException();
+    }
+    this.activated = true;
+  }
+
+  public void deactivate(){
+    if (!this.activated) {
+      throw new IllegalArgumentException();
+    }
+    this.activated = false;
+  }
+
   public String getName() {
     return name;
   }
